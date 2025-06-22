a7343a5f448b2a8a3f5fb14153a2517cd8614603
deepsikha05 <deepsikha1104@gmail.com>
2025-06-22 12:47:15 +0530

    Re-added SingletonPattern_EX1 under WEEK 1 Solutions

---
+++ WEEK1_Solutions/SingletonPattern_EX1/Logger.cs
@@ -0,0 +1,30 @@
+public class Logger
+{
+    private static Logger instance;
+    private static readonly object lockObj = new object();
+
+    private Logger()
+    {
+        Console.WriteLine("Logger Initialized.");
+    }
+
+    public static Logger GetInstance()
+    {
+        if (instance == null)
+        {
+            lock (lockObj)
+            {
+                if (instance == null)
+                {
+                    instance = new Logger();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine($"Log: {message}");
+    }
+}
---
+++ WEEK1_Solutions/SingletonPattern_EX1/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Logger logger1 = Logger.GetInstance();
+        logger1.Log("First log message.");
+
+        Logger logger2 = Logger.GetInstance();
+        logger2.Log("Second log message.");
+
+        Console.WriteLine(Object.ReferenceEquals(logger1, logger2)
+            ? "Same Logger instance."
+            : "Different Logger instances.");
+    }
+}
