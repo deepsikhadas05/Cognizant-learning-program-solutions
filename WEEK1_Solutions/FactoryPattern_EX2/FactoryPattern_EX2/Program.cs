e2cb58bffed2532b3bfc99f95dd9f787bc3283d7
deepsikha05 <deepsikha1104@gmail.com>
2025-06-22 12:56:00 +0530

    Add Factory Method Pattern example under WEEK 1 Solutions

---
+++ WEEK1_Solutions/FactoryPattern_EX2/FactoryPattern_EX2/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        DocumentFactory factory;
+
+        factory = new WordDocumentFactory();
+        IDocument wordDoc = factory.CreateDocument();
+        wordDoc.Open();
+
+        factory = new PdfDocumentFactory();
+        IDocument pdfDoc = factory.CreateDocument();
+        pdfDoc.Open();
+
+        factory = new ExcelDocumentFactory();
+        IDocument excelDoc = factory.CreateDocument();
+        excelDoc.Open();
+    }
+}
